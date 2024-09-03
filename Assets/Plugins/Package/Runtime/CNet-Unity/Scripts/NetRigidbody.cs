5fe790f750803082952049e458e3b2316fa20445
Cole <93949583+Monstroe@users.noreply.github.com>
2024-09-03 02:58:23 -0400

    Latest

--- Assets/Plugins/Package/Runtime/CNet-Unity/Scripts/NetRigidbody.cs
+++ Assets/Plugins/Package/Runtime/CNet-Unity/Scripts/NetRigidbody.cs
@@ -7,8 +7,14 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class NetRigidbody : MonoBehaviour
 {
-    public Vector3 velocity;
-    public Vector3 angularVelocity;
+    [SerializeField] private bool syncVelocity = false;
+    [SerializeField] private bool syncAngularVelocity = false;
+    [Space]
+    [SerializeField] private int syncRate = 10;
+
+    public Vector3 Velocity { get => rb.velocity; set => SyncVelocity(value, syncVelocity, true); }
+    public Vector3 AngularVelocity { get => rb.angularVelocity; set => SyncAngularVelocity(value, syncAngularVelocity, true); }
+
     private SyncedTransform syncedTransform;
     private Rigidbody rb;
 
@@ -17,7 +23,7 @@ public class NetRigidbody : MonoBehaviour
         syncedTransform = GetComponent<SyncedTransform>();
         rb = GetComponent<Rigidbody>();
 
-        if (!NetManager.Instance.IsHost)
+        if (!NetManager.Instance.IsHost && syncedTransform.SyncOn == SyncOn.ServerSide)
         {
             rb.isKinematic = true;
         }
@@ -26,13 +32,101 @@ public class NetRigidbody : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        StartCoroutine(UpdateCoroutine());
+    }
 
+    private IEnumerator UpdateCoroutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1f / syncRate);
+
+            Vector3 newPosition;
+            Quaternion newRotation;
+
+            if (rb.isKinematic)
+            {
+                newPosition = syncedTransform.Position + Velocity * (1f / syncRate);
+                newRotation = syncedTransform.Rotation * Quaternion.Euler(AngularVelocity * (1f / syncRate));
+            }
+            else
+            {
+                newPosition = syncedTransform.Position;
+                newRotation = syncedTransform.Rotation;
+            }
+
+            syncedTransform.Position = newPosition;
+            syncedTransform.Rotation = newRotation;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (NetManager.Instance.Connected)
+        {
+            if (syncVelocity)
+            {
+                transform.position += rb.velocity * Time.deltaTime;
+            }
 
+            if (syncAngularVelocity)
+            {
+                transform.rotation *= Quaternion.Euler(rb.angularVelocity * Time.deltaTime);
+            }
+        }
+    }
+
+    internal void SyncVelocity(Vector3 velocity, bool sync, bool force = false)
+    {
+        if (!syncVelocity && !force)
+        {
+            Debug.LogError("<color=red><b>CNet</b></color>: NetRigidbody is not set to sync velocity");
+            return;
+        }
+
+        if (sync)
+        {
+            using (NetPacket packet = new NetPacket(NetManager.Instance.System, PacketProtocol.UDP))
+            {
+                packet.Write((short)ServiceType.Transform);
+                packet.Write((short)syncedTransform.NetID);
+                packet.Write((byte)TransformService.TransformServiceType.Velocity);
+                packet.SerializeStruct((NetVector3)velocity);
+                NetManager.Instance.Send(packet, PacketProtocol.UDP);
+            }
+        }
+
+        if (!sync || NetManager.Instance.IsHost || syncedTransform.SyncOn == SyncOn.ClientSide)
+        {
+            rb.velocity = velocity;
+        }
+    }
+
+    internal void SyncAngularVelocity(Vector3 angularVelocity, bool sync, bool force = false)
+    {
+        if (!syncAngularVelocity && !force)
+        {
+            Debug.LogError("<color=red><b>CNet</b></color>: NetRigidbody is not set to sync angular velocity");
+            return;
+        }
+
+        if (sync)
+        {
+            using (NetPacket packet = new NetPacket(NetManager.Instance.System, PacketProtocol.UDP))
+            {
+                packet.Write((short)ServiceType.Transform);
+                packet.Write((short)syncedTransform.NetID);
+                packet.Write((byte)TransformService.TransformServiceType.AngularVelocity);
+                packet.SerializeStruct((NetVector3)angularVelocity);
+                NetManager.Instance.Send(packet, PacketProtocol.UDP);
+            }
+        }
+
+        if (!sync || NetManager.Instance.IsHost || syncedTransform.SyncOn == SyncOn.ClientSide)
+        {
+            rb.angularVelocity = angularVelocity;
+        }
     }
 
     public void AddForce(Vector3 force)
@@ -42,7 +136,7 @@ public class NetRigidbody : MonoBehaviour
 
     public void AddForce(Vector3 force, ForceMode mode)
     {
-        if (NetManager.Instance.IsHost)
+        if (NetManager.Instance.IsHost || syncedTransform.SyncOn == SyncOn.ClientSide)
         {
             rb.AddForce(force, mode);
         }
--- Assets/Plugins/Package/Runtime/CNet-Unity/Scripts/Synced/SyncedTransform.cs
+++ Assets/Plugins/Package/Runtime/CNet-Unity/Scripts/Synced/SyncedTransform.cs
@@ -5,41 +5,18 @@ using UnityEngine;
 
 public class SyncedTransform : SyncedObject
 {
+    public SyncOn SyncOn { get => syncOn; set => syncOn = value; }
+    public Vector3 Position { get => transform.position; set => SyncPosition(value, true); }
+    public Quaternion Rotation { get => transform.rotation; set => SyncRotation(value, true); }
+    public Vector3 Scale { get => transform.localScale; set => SyncScale(value, true); }
+
     [SerializeField] private SyncOn syncOn;
     [Space]
     [SerializeField] private bool syncPosition;
     [SerializeField] private bool syncRotation;
     [SerializeField] private bool syncScale;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        /*if (NetManager.Instance.IsHost || syncOn == SyncOn.ClientSide)
-        {
-            if (syncPosition)
-            {
-                SendPosition(transform.position);
-            }
-
-            if (syncRotation)
-            {
-                SendRotation(transform.rotation);
-            }
-
-            if (syncScale)
-            {
-                SendScale(transform.localScale);
-            }
-        }*/
-    }
-
-    public void SyncPosition(Vector3 pos)
+    internal void SyncPosition(Vector3 pos, bool sync)
     {
         if (!syncPosition)
         {
@@ -47,27 +24,25 @@ public class SyncedTransform : SyncedObject
             return;
         }
 
-        transform.position = pos;
-
-        if (NetManager.Instance.IsHost)
+        if (sync)
         {
-            SendPosition(pos);
+            using (NetPacket packet = new NetPacket(NetManager.Instance.System, PacketProtocol.UDP))
+            {
+                packet.Write((short)ServiceType.Transform);
+                packet.Write((short)NetID);
+                packet.Write((byte)TransformService.TransformServiceType.Position);
+                packet.SerializeStruct((NetVector3)pos);
+                NetManager.Instance.Send(packet, PacketProtocol.UDP);
+            }
+        }
+
+        if (!sync || NetManager.Instance.IsHost || syncOn == SyncOn.ClientSide)
+        {
+            transform.position = pos;
         }
     }
 
-    private void SendPosition(Vector3 pos)
-    {
-        using (NetPacket packet = new NetPacket(NetManager.Instance.System, PacketProtocol.UDP))
-        {
-            packet.Write((short)ServiceType.Transform);
-            packet.Write((short)NetID);
-            packet.Write((byte)TransformService.TransformServiceType.Position);
-            packet.SerializeStruct<NetVector3>((NetVector3)pos);
-            NetManager.Instance.Send(packet, PacketProtocol.UDP);
-        }
-    }
-
-    public void SyncRotation(Quaternion rot)
+    internal void SyncRotation(Quaternion rot, bool sync)
     {
         if (!syncRotation)
         {
@@ -75,27 +50,25 @@ public class SyncedTransform : SyncedObject
             return;
         }
 
-        transform.rotation = rot;
-
-        if (NetManager.Instance.IsHost)
+        if (sync)
         {
-            SendRotation(rot);
+            using (NetPacket packet = new NetPacket(NetManager.Instance.System, PacketProtocol.UDP))
+            {
+                packet.Write((short)ServiceType.Transform);
+                packet.Write((short)NetID);
+                packet.Write((byte)TransformService.TransformServiceType.Rotation);
+                packet.SerializeStruct((NetQuaternion)rot);
+                NetManager.Instance.Send(packet, PacketProtocol.UDP);
+            }
+        }
+
+        if (!sync || NetManager.Instance.IsHost || syncOn == SyncOn.ClientSide)
+        {
+            transform.rotation = rot;
         }
     }
 
-    private void SendRotation(Quaternion rot)
-    {
-        using (NetPacket packet = new NetPacket(NetManager.Instance.System, PacketProtocol.UDP))
-        {
-            packet.Write((short)ServiceType.Transform);
-            packet.Write((short)NetID);
-            packet.Write((byte)TransformService.TransformServiceType.Rotation);
-            packet.SerializeStruct<NetQuaternion>((NetQuaternion)rot);
-            NetManager.Instance.Send(packet, PacketProtocol.UDP);
-        }
-    }
-
-    public void SyncScale(Vector3 scl)
+    internal void SyncScale(Vector3 scl, bool sync)
     {
         if (!syncScale)
         {
@@ -103,23 +76,21 @@ public class SyncedTransform : SyncedObject
             return;
         }
 
-        transform.localScale = scl;
-
-        if (NetManager.Instance.IsHost)
+        if (sync)
         {
-            SendScale(scl);
+            using (NetPacket packet = new NetPacket(NetManager.Instance.System, PacketProtocol.UDP))
+            {
+                packet.Write((short)ServiceType.Transform);
+                packet.Write((short)NetID);
+                packet.Write((byte)TransformService.TransformServiceType.Scale);
+                packet.SerializeStruct((NetVector3)scl);
+                NetManager.Instance.Send(packet, PacketProtocol.UDP);
+            }
         }
-    }
 
-    private void SendScale(Vector3 scl)
-    {
-        using (NetPacket packet = new NetPacket(NetManager.Instance.System, PacketProtocol.UDP))
+        if (!sync || NetManager.Instance.IsHost || syncOn == SyncOn.ClientSide)
         {
-            packet.Write((short)ServiceType.Transform);
-            packet.Write((short)NetID);
-            packet.Write((byte)TransformService.TransformServiceType.Scale);
-            packet.SerializeStruct<NetVector3>((NetVector3)scl);
-            NetManager.Instance.Send(packet, PacketProtocol.UDP);
+            transform.localScale = scl;
         }
     }
 }
